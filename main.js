6f5dc6dfc6954961a2152c2784d7f722181cdb52
gss-patricia <gss.patricia@gmail.com>
2023-12-27 20:15:10 +0000

    video 1.4

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
+function alternarSubmenu(item, mostrar) {
+  const submenu = item.querySelector(".submenu");
+
+  if (submenu) {
+    submenu.style.display = mostrar ? "block" : "none";
+  }
+}
+
 //selecionar todos os cabecalho__lista-item
-document.querySelectorAll(".cabecalho__lista-item").forEach(
-    (item,
-    () => {
-    // adicionar um ouvinte mouseover
-    item.addEventListener("mouseover", () => alternarSubmenu(item, true));
+document.querySelectorAll(".cabecalho__lista-item").forEach((item) => {
+  // adicionar um ouvinte mouseover
+  item.addEventListener("mouseover", () => alternarSubmenu(item, true));
 
-    // adicionar um ouvinte mouseout
-    item.addEventListener("mouseout", () => alternarSubmenu(item, false));
+  // adicionar um ouvinte mouseout
+  item.addEventListener("mouseout", () => alternarSubmenu(item, false));
 
-    item.addEventListener("click", () => {
-        const submenu = item.querySelector(".submenu");
+  item.addEventListener("click", () => {
+    const submenu = item.querySelector(".submenu");
 
-        const isDisplayed = submenu.style.display === "block";
+    const isDisplayed = submenu.style.display === "block";
 
-        alternarSubmenu(item, !isDisplayed);
-    });
-  })
-);
+    alternarSubmenu(item, !isDisplayed);
+  });
+});
